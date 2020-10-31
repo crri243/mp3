9f7706ad1dd567322a1300854354cc23c6c7eeb6
crri243 <70342891+crri243@users.noreply.github.com>
2020-10-30 20:14:52 -0400

    test file for q4 again

--- jenkins/core/src/test/java/jenkins/model/IdfromFilenameTest.java
+++ jenkins/core/src/test/java/jenkins/model/IdfromFilenameTest.java
@@ -1,5 +1,19 @@
 package jenkins.model;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 public class IdfromFilenameTest {
-
+	
+	@Test
+	public void BeginTest1 () {
+		IdStrategy.CaseSensitive newCase = new IdStrategy.CaseSensitive();
+		assertEquals(newCase.idFromFilename("this"), "this");
+	}
+	
+	@Test
+	public void BeginTest2 () {
+		IdStrategy.CaseSensitive newCase = new IdStrategy.CaseSensitive();
+		assertNotEquals(newCase.idFromFilename("$"), "this");
+	}
 }
